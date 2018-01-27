67e2ae2190a4ecb2de6c43aec1213147c767dd98
Cat Stevens <catb0t@protonmail.ch>
2018-01-27 14:39:32 -0500

    update tests

--- src/test/test_metadata.c
+++ src/test/test_metadata.c
@@ -2,5 +2,10 @@
 #include "../virtaddr.h"
 
 Test(metadata, count) {
-  
+  virtaddr_t a = alloc(triple_atom_t*, 2);
+  a[0] = lifetimes_make_first_triple(3, 10, false);
+  a[1] = NULL;
+
+  cr_assert_eq(3, attributes_len_real( a ));
+  cr_assert_eq(10, attributes_len_virtual( a ));
 }
